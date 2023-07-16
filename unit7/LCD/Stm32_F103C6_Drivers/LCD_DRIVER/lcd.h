7a0e63ee13e6a8ba16c85d6575cc575026f6d90d
Adhoum <adhoum@gmail.com>
2023-07-16 22:59:35 +0300

    unit7 lcd update

--- unit7/LCD/Stm32_F103C6_Drivers/LCD_DRIVER/lcd.h
+++ unit7/LCD/Stm32_F103C6_Drivers/LCD_DRIVER/lcd.h
@@ -1,57 +1,110 @@
 /*
- * lcd.h
+ * LCD.h
  *
- * Created: 4/22/2021 3:01:25 PM
- *  Author: Marco
+ *  Created on: Jul 6, 2023
+ *      Author: adham
  */
 
-
-#ifndef LCD_H_
-#define LCD_H_
-
+#ifndef HAL_LCD_LCD_H_
+#define HAL_LCD_LCD_H_
+//-----------------------------
+//Includes
+#include"../../includes/std_macros.h"
+#include"../../includes/std_types.h"
+#include"../../includes/stm32_f103_x6.h"
+#include"../../MCAL/GPIO/GPIO.h"
+//-----------------------------
 #define F_CPU 1000000UL
-#include "stm32f103x6.h"
-#include "Stm32_F103C6_gpio_driver.h"
+#define LCD_DATA_BITS_MODE 8
 
-#define LCD_CTRL GPIOA
-#define RS_SWITCH GPIO_PIN_8
-#define RW_SWITCH GPIO_PIN_9
-#define EN_SWITCH GPIO_PIN_10
-#define DATA_shift 4 // in 8 bit mode 0 and in 4 bit mode 4
+#if((LCD_DATA_BITS_MODE != 4) && (LCD_DATA_BITS_MODE != 8))
 
-//#define EIGHT_BIT_MODE
-//#define FOUR_BIT_MODE
+#error "Number of Data bits should be equal to 4 or 8"
 
-#define LCD_FUNCTION_8BIT_2LINES   					(0x38)
-#define LCD_FUNCTION_4BIT_2LINES   					(0x28)
-#define LCD_MOVE_DISP_RIGHT       					(0x1C)
-#define LCD_MOVE_DISP_LEFT   						(0x18)
-#define LCD_MOVE_CURSOR_RIGHT   					(0x14)
-#define LCD_MOVE_CURSOR_LEFT 	  					(0x10)
-#define LCD_DISP_OFF   								(0x08)
-#define LCD_DISP_ON_CURSOR   						(0x0E)
-#define LCD_DISP_ON_CURSOR_BLINK   					(0x0F)
-#define LCD_DISP_ON_BLINK   						(0x0D)
-#define LCD_DISP_ON   								(0x0C)
-#define LCD_ENTRY_DEC   							(0x04)
-#define LCD_ENTRY_DEC_SHIFT   						(0x05)
-#define LCD_ENTRY_INC_   							(0x06)
-#define LCD_ENTRY_INC_SHIFT   						(0x07)
-#define LCD_BEGIN_AT_FIRST_ROW						(0x80)
-#define LCD_BEGIN_AT_SECOND_ROW						(0xC0)
-#define LCD_CLEAR_SCREEN							(0x01)
-#define LCD_ENTRY_MODE								(0x06)
+#endif
 
-GPIO_PinConfig_t PinCfg ;
+//-----------------------------
+//LCD Pins
+#define LCD_DATA_PORT		GPIOA
+#define LCD_COMM_PORT		GPIOA
 
-void LCD_INIT();
-void LCD_WRITE_COMMAND(unsigned char command);
-void LCD_WRITE_CHAR(unsigned char character);
-void LCD_WRITE_STRING(char* string);
-void LCD_check_lcd_isbusy(void);
-void LCD_lcd_kick(void);
-void LCD_clear_screen();
-void LCD_GOTO_XY(unsigned char line, unsigned char position);
+#define LCD_COMM_RS			GPIO_PIN8
+#define LCD_COMM_RW			GPIO_PIN9
+#define LCD_COMM_EN			GPIO_PIN10
+
+#define LCD_DATA_D0			GPIO_PIN0
+#define LCD_DATA_D1			GPIO_PIN1
+#define LCD_DATA_D2			GPIO_PIN2
+#define LCD_DATA_D3			GPIO_PIN3
+#define LCD_DATA_D4			GPIO_PIN4
+#define LCD_DATA_D5			GPIO_PIN5
+#define LCD_DATA_D6			GPIO_PIN6
+#define LCD_DATA_D7			GPIO_PIN7
+
+//-----------------------------
 
 
-#endif /* LCD_H_ */
+
+
+//-----------------------------
+//LCD Commands
+#define LCD_CLR_SCREEN					(0x01)
+#define LCD_RETURN_HOME					(0x02)
+#define	LCD_ENTERY_MODE					(0x06)
+#define LCD_DISPLAY_CURSOR_ON			(0x0E)
+#define LCDC_DISPLAY_CURSOR_ON_BLINK 	(0x0F)
+#define LCD_BEGIN_AT_FIRIST_ROW			(0x80)
+#define LCD_8BIT_MODE_SELECT			(0x38)
+#define LCD_4BIT_MODE_SELECT_1			(0x33)
+#define LCD_4BIT_MODE_SELECT_2			(0x32)
+
+//-----------------------------
+
+
+
+
+//-----------------------------
+//LCD Rows and Columns
+enum Rows
+{
+	LCD_ROW1=0,
+	LCD_ROW2,
+};
+
+enum Columns
+{
+	LCD_Column1=0, LCD_Column2, LCD_Column3, LCD_Column4, LCD_Column5,
+	LCD_Column6, LCD_Column7, LCD_Column8, LCD_Column9, LCD_Column10,
+	LCD_Column11, LCD_Column12, LCD_Column13, LCD_Column14, LCD_Column15,
+	LCD_Column16, LCD_Column17, LCD_Column18, LCD_Column19, LCD_Column20,
+	LCD_Column21, LCD_Column22, LCD_Column23, LCD_Column24, LCD_Column25,
+	LCD_Column26,	LCD_Column27, LCD_Column28, LCD_Column29, LCD_Column30,
+	LCD_Column31, LCD_Column32
+	};
+
+//-----------------------------
+
+
+
+//-----------------------------
+/*======================================================
+ * 													   *
+ * 			APIs Supported by "HAL LCD DRIVER"       *
+ * 				                                       *
+ *======================================================
+ */
+void HAL_LCD_Display_Char(uint8_t data);
+void HAL_LCD_Send_Command(uint8_t comm);
+void HAL_LCD_Clear_Screen(void);
+void HAL_LCD_Init(void);
+void HAL_LCD_GoToXY(uint8_t row , uint8_t col);
+void HAL_LCD_Display_String(uint8_t *ptr_str);
+void HAL_LCD_Display_Number(uint8_t number);
+void HAL_LCD_Display_MultiDigNum(uint32_t number);
+
+//-----------------------------
+
+
+
+
+#endif /* HAL_LCD_LCD_H_ */
